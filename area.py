a24ee0a77896c42cd55dc6ecf06920f233f58bad
vishalkumar43 <98085750+vishalkumar43@users.noreply.github.com>
2022-04-11 20:03:42 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 19:22:43 2022
+
+@author: HP
+"""
+
+PI=3.14
+r=float(input('radius of the circle:'))
+area=PI*r*r
+print("area of the circle =%.2f"%area)
+
+ 
